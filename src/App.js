29716df41059e0e1fe5121657797e1abbab70f05
Маслёнченко Илья <inmaslyo@mts.ru>
2025-05-31 15:43:55 +0300

    refactor: change paragraph text in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Homework-01</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My first React.js application.
         </p>
         <a
           className="App-link"
